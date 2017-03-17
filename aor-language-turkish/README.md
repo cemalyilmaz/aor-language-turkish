268828a3437c835b8f8b96cd928690956908224f
mhcemalyilmaz <cemal@mindhive.me>
2017-03-17 18:23:12 +0300

    initial

---
+++ aor-language-turkish/README.md
@@ -0,0 +1,28 @@
+# Turkish Translations for Admin-on-rest
+
+Turkish translations for [admin-on-rest](https://github.com/marmelab/admin-on-rest), the frontend framework for building admin applications on top of REST services.
+
+
+## Installation
+
+```sh
+npm install --save aor-language-turkish
+```
+
+## Usage
+
+```js
+import trMessages from 'aor-language-turkish';
+
+const messages = {
+    'tr': trMessages,
+};
+
+<Admin locale="tr" messages={messages}>
+  ...
+</Admin>
+```
+
+## License
+
+This translation is licensed under the [MIT Licence](LICENSE).
---
+++ aor-language-turkish/index.js
@@ -0,0 +1,65 @@
+module.exports = {
+  aor: {
+        action: {
+            delete: 'Sil',
+            show: 'Göster',
+            list: 'Listele',
+            save: 'Kaydet',
+            create: 'Yenı',
+            edit: 'Düzenle',
+            cancel: 'Vazgeç',
+            refresh: 'Yenile',
+            add_filter: 'Filtre ekle',
+            remove_filter: 'Filtreyi kaldır',
+        },
+        boolean: {
+            true: 'Evet',
+            false: 'Hayır',
+        },
+        page: {
+            list: '%{name} Listele',
+            edit: '%{name} #%{id}',
+            show: '%{name} #%{id}',
+            create: 'Yeni %{name}',
+            delete: 'Sil %{name} #%{id}',
+            dashboard: 'Dashboard',
+        },
+        input: {
+            image: {
+                upload_several: 'Buraya dosyaları sürükleyin ya da seçim yapın.',
+                upload_single: 'Buraya dosyayı sürükleyin ya da seçim yapın.',
+            },
+        },
+        message: {
+            yes: 'Evet',
+            no: 'Hayır',
+            are_you_sure: 'Emin misiniz?',
+            about: 'Hakkında',
+        },
+        navigation: {
+            page_out_of_boundaries: 'Sayfa %{page} bulunamadı',
+            page_out_from_end: 'Son sayfadasınız',
+            page_out_from_begin: 'İlk sayfadasınız',
+            page_range_info: '%{offsetBegin}-%{offsetEnd}  %{total}',
+            next: 'Sonraki',
+            prev: 'Önceki',
+        },
+        auth: {
+            username: 'Kullanıcı Adı',
+            password: 'Şifre',
+            sign_in: 'Giriş yap',
+            sign_in_error: 'Authentication failed, please retry',
+            logout: 'Logout',
+        },
+        notification: {
+            updated: 'Güncellendi.',
+            created: 'Oluşturuldu.',
+            deleted: 'Silindi.',
+            item_doesnt_exist: 'Öğe bulunmuyor.',
+            http_error: 'Bağlantı hatası.',
+        },
+        validation: {
+            required: 'Zorunlu alan',
+        },
+    },
+};
